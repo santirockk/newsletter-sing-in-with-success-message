2810e057630011238a48ec0912faf680a7068758
santirockk <santiquu@gmail.com>
2023-12-20 19:37:01 -0500

    1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Frontend-Mentor-newletter-sign-in-with-success-message/',
+  base: '/newsletter-sign-in-with-success-message/',
 })
